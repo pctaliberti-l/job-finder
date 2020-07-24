8362b7e3231a6da6dde645dee578d79686309a50
pctaliberti-l <pauloct-lovo@live.com>
2020-07-24 15:55:34 -0300

    adicionado página ver vagas + rotas

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,10 +4,25 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
 
+// rota de teste
 router.get("/test", (req, res) => {
   res.send("deu certo");
 });
 
+// detalhe da vaga -> view/1, view/2
+router.get("/view/:id", (req, res) =>
+  Job.findOne({
+    where: { id: req.params.id },
+  })
+    .then((job) => {
+      res.render("view", {
+        job,
+      });
+    })
+    .catch((err) => console.log(err))
+);
+
+// form da rota de envio
 router.get("/add", (req, res) => {
   res.render("add");
 });
